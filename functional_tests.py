dd6866878be2554499c5a16ab83034b9ad521e2d
Tiago Almeida <tyagow@hotmail.com.br>
2016-08-26 14:17:51 -0300

    unittest added and test file renamed

---
+++ functional_tests.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import unittest
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        ''' Edith has heard about a cool new online to-do app. She goes to check ou its home page'''
+        self.browser.get('http://localhost:8000')
+
+        # She Notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('finish test!')
+
+        #She is inviter to enter a to-do item straight away
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
