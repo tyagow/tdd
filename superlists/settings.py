3dc1c6aba3cd2b4c2d11b948c68d86a8f526b2bf
Tiago Almeida <tyagow@hotmail.com.br>
2016-08-28 18:01:25 -0300

    Model for list Items and associated migration

--- superlists/settings.py
+++ superlists/settings.py
@@ -40,15 +40,16 @@ INSTALLED_APPS = [
     'lists',
 ]
 
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
+MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+    'django.middleware.security.SecurityMiddleware',
+)
 
 ROOT_URLCONF = 'superlists.urls'
 
