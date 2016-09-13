9be2817237d81be35dcd8bd9e447343d66e686f1
Tiago Almeida <tyagow@hotmail.com.br>
2016-09-13 17:54:52 -0300

    Start to change function base view to class base view

--- lists/tests/test_view.py
+++ lists/tests/test_view.py
@@ -1,7 +1,11 @@
 from django.test import TestCase
 from django.utils.html import escape
 
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
+    ExistingListItemForm, ItemForm,
+)
+
 from lists.models import Item, List
 from unittest import skip
 
@@ -81,7 +85,7 @@ class ListViewTest(TestCase):
     def test_displays_item_form(self):
         list_ = List.objects.create()
         response = self.client.get('/lists/%d/' % (list_.id,))
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
         self.assertContains(response, 'name="text"')
 
 
@@ -142,13 +146,13 @@ class ListViewTest(TestCase):
 
     def test_for_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_for_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    @skip
+
     def test_duplicate_item_validation_erros_end_up_on_lists_pafe(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text='textey')
@@ -156,7 +160,7 @@ class ListViewTest(TestCase):
             '/lists/%d/' %(list1.id,),
             data={'text': 'textey'}
         )
-        expected_error = escape("You've already got this in your list")
+        expected_error = escape(DUPLICATE_ITEM_ERROR)
         self.assertContains(response, expected_error)
-        self.assertTemplatedUsed(response, 'list.html')
+        self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
--- superlists/urls.py
+++ superlists/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from lists.views import HomePageView
 from lists import views as list_views
 from lists import urls as list_urls
 
 urlpatterns = [
-    url(r'^$', list_views.home_page, name='home'),
+    url(r'^$', HomePageView.as_view(), name='home'),
     url(r'^lists/', include(list_urls)),
 #   url(r'^admin/', admin.site.urls),
 ]
